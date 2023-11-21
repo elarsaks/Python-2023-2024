df25d120b751ea2813be1debbceee2957bb40ebc
Elar Saks <elarsaks@gmail.com>
2023-11-21 18:11:46 +0200

    Solve quadratic

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part01-e08_solve_quadratic/src/solve_quadratic.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part01-e08_solve_quadratic/src/solve_quadratic.py
@@ -2,12 +2,21 @@
 
 import math
 
+
 def solve_quadratic(a, b, c):
-    return (0,0)
+    # Calculate the discriminant
+    discriminant = b**2 - 4*a*c
+
+    # Calculate two solutions
+    root1 = (-b + math.sqrt(discriminant)) / (2*a)
+    root2 = (-b - math.sqrt(discriminant)) / (2*a)
+
+    return (root1, root2)
 
 
 def main():
     pass
+
 
 if __name__ == "__main__":
     main()
