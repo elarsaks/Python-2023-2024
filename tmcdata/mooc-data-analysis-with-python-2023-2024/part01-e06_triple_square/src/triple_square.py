3755df3e6123e5301619ec96100a3240e70f34e8
Elar Saks <elarsaks@gmail.com>
2023-11-21 17:56:18 +0200

    Solve shapes.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part01-e06_triple_square/src/triple_square.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part01-e06_triple_square/src/triple_square.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 
-
 def main():
-    pass
+    def triple(n):
+        return n * 3
+
+    def square(n):
+        return n ** 2
+
+    for i in range(1, 11):
+        print(f"triple({i})=={triple(i)} square({i})=={square(i)}")
+
+        if square(i) > triple(i):
+            break
+
 
 if __name__ == "__main__":
     main()
--- tmcdata/mooc-data-analysis-with-python-2023-2024/part01-e07_areas_of_shapes/src/areas_of_shapes.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part01-e07_areas_of_shapes/src/areas_of_shapes.py
@@ -4,7 +4,28 @@ import math
 
 
 def main():
-    # enter you solution here
+    while True:
+        shape = input("Choose a shape (triangle, rectangle, circle): ").lower()
+
+        if shape == "":
+            break
+        elif shape == "triangle":
+            base = float(input("Give base of the triangle: "))
+            height = float(input("Give height of the triangle: "))
+            area = 0.5 * base * height
+            print(f"The area is {area:.6f}")
+        elif shape == "rectangle":
+            width = float(input("Give width of the rectangle: "))
+            height = float(input("Give height of the rectangle: "))
+            area = width * height
+            print(f"The area is {area:.6f}")
+        elif shape == "circle":
+            radius = float(input("Give radius of the circle: "))
+            area = math.pi * radius ** 2
+            print(f"The area is {area:.6f}")
+        else:
+            print("Unknown shape!")
+
 
 if __name__ == "__main__":
     main()
