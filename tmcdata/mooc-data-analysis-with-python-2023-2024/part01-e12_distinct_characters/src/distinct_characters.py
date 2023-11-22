62ac478afe9072a003e9f24582a6baacbd512f49
Elar Saks <elarsaks@gmail.com>
2023-11-22 12:35:45 +0200

    Do distinct characters.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part01-e12_distinct_characters/src/distinct_characters.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part01-e12_distinct_characters/src/distinct_characters.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 
 def distinct_characters(L):
-    return {}
+    result = {}
+    for string in L:
+        # Convert the string into a set to get distinct characters
+        distinct_chars = set(string)
+        # The number of distinct characters is the length of the set
+        result[string] = len(distinct_chars)
+    return result
+
 
 def main():
     print(distinct_characters(["check", "look", "try", "pop"]))
+
 
 if __name__ == "__main__":
     main()
