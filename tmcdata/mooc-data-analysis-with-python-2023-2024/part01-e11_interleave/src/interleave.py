1c0a79abbd0a365fd46cdc47478e99db822a03f8
Elar Saks <elarsaks@gmail.com>
2023-11-22 12:23:35 +0200

    Do interleaved.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part01-e11_interleave/src/interleave.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part01-e11_interleave/src/interleave.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
 def interleave(*lists):
-    return []
+    interleaved = []
+    for items in zip(*lists):
+        interleaved.extend(items)
+    return interleaved
+
 
 def main():
     print(interleave([1, 2, 3], [20, 30, 40], ['a', 'b', 'c']))
+
 
 if __name__ == "__main__":
     main()
