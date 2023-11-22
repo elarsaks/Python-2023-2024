fe1940a396087deb0e7a206d8a69c83a1d24e67a
Elar Saks <elarsaks@gmail.com>
2023-11-22 12:52:29 +0200

    Find matching.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part01-e14_find_matching/src/find_matching.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part01-e14_find_matching/src/find_matching.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
 def find_matching(L, pattern):
-    return []
+    matching_indices = []
+    for index, string in enumerate(L):
+        if pattern in string:
+            matching_indices.append(index)
+    return matching_indices
+
 
 def main():
     pass
+
 
 if __name__ == "__main__":
     main()
