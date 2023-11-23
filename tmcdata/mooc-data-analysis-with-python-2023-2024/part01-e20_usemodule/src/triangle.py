1c1b887ea7b408b98c6c5a920dc96fa21137d281
Elar Saks <elarsaks@gmail.com>
2023-11-23 11:39:06 +0200

    Do "use module". Finnish part 1.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part01-e20_usemodule/src/triangle.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part01-e20_usemodule/src/triangle.py
@@ -1,1 +1,38 @@
-# Enter you module contents here
+"""
+triangle_math module
+
+This module provides functions to calculate properties of a right-angled triangle.
+"""
+
+__version__ = "1.0"
+__author__ = "Your Name"
+
+import math
+
+
+def hypotenuse(side1, side2):
+    """
+    Calculate the length of the hypotenuse in a right-angled triangle.
+
+    Parameters:
+    side1 (float): Length of one side of the triangle.
+    side2 (float): Length of the other side of the triangle.
+
+    Returns:
+    float: Length of the hypotenuse.
+    """
+    return math.sqrt(side1**2 + side2**2)
+
+
+def area(side1, side2):
+    """
+    Calculate the area of a right-angled triangle.
+
+    Parameters:
+    side1 (float): Length of one side of the triangle.
+    side2 (float): Length of the other side of the triangle.
+
+    Returns:
+    float: Area of the triangle.
+    """
+    return 0.5 * side1 * side2
--- tmcdata/mooc-data-analysis-with-python-2023-2024/part01-e20_usemodule/src/usemodule.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part01-e20_usemodule/src/usemodule.py
@@ -6,8 +6,20 @@ try:
 except ModuleNotFoundError:
     import triangle
 
+
 def main():
     # Call the functions from here
+    side1 = 3
+    side2 = 4
+
+    hyp = triangle.hypotenuse(side1, side2)
+    print(
+        f"The hypotenuse of a triangle with sides {side1} and {side2} is {hyp}")
+
+    tri_area = triangle.area(side1, side2)
+    print(
+        f"The area of a right-angled triangle with sides {side1} and {side2} is {tri_area}")
+
 
 if __name__ == "__main__":
     main()
