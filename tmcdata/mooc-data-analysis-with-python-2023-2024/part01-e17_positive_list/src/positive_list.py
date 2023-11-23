9f6af96babfdd0abcc60a3ce037aa8c84e5759d9
Elar Saks <elarsaks@gmail.com>
2023-11-23 10:00:00 +0200

    Do a positive list.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part01-e17_positive_list/src/positive_list.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part01-e17_positive_list/src/positive_list.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 
 def positive_list(L):
-    return []
+    # Define a function that checks if a number is positive
+    def is_positive(n):
+        return n > 0
+
+    # Use filter to apply the is_positive function to the list of numbers
+    filtered_numbers = filter(is_positive, L)
+
+    # Convert the filter object to a list and return it
+    return list(filtered_numbers)
+
 
 def main():
     pass
+
 
 if __name__ == "__main__":
     main()
