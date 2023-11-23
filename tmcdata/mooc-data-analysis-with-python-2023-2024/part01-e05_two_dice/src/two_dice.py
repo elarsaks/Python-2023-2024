d46981371da2eb0a9deca422ba68a3707a04b86b
Elar Saks <elarsaks@gmail.com>
2023-11-23 09:25:29 +0200

    Place a solution into correct file.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part01-e05_two_dice/src/two_dice.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part01-e05_two_dice/src/two_dice.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 
 def main():
-    # Create a list of tuples where the sum of the dice rolls is 5
-    combinations = [(dice1, dice2) for dice1 in range(1, 7)
-                    for dice2 in range(1, 7) if dice1 + dice2 == 5]
-
-    # Print each combination
-    for combination in combinations:
-        print(combination)
+    # Iterate over all possible combinations of two dice rolls
+    for dice1 in range(1, 7):
+        for dice2 in range(1, 7):
+            # Check if the sum of the two dice rolls equals 5
+            if dice1 + dice2 == 5:
+                print(f"({dice1}, {dice2})")
 
 
 if __name__ == "__main__":
--- tmcdata/mooc-data-analysis-with-python-2023-2024/part01-e15_two_dice_comprehension/src/two_dice_comprehension.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part01-e15_two_dice_comprehension/src/two_dice_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def main():
-    pass
+    # Create a list of tuples where the sum of the dice rolls is 5
+    combinations = [(dice1, dice2) for dice1 in range(1, 7)
+                    for dice2 in range(1, 7) if dice1 + dice2 == 5]
+
+    # Print each combination
+    for combination in combinations:
+        print(combination)
+
 
 if __name__ == "__main__":
     main()
