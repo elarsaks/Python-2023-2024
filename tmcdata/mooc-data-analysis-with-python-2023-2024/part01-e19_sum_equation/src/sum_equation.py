5b814957fa867e6ad590a30282b2b408f5a9fea9
Elar Saks <elarsaks@gmail.com>
2023-11-23 10:20:02 +0200

    Do some_equation -> reduce func.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part01-e19_sum_equation/src/sum_equation.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part01-e19_sum_equation/src/sum_equation.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 
 def sum_equation(L):
-    return ""
+    if not L:  # Check if the list is empty
+        return "0 = 0"
+    else:
+        # Join numbers with " + ", calculate sum, and format the string
+        equation = " + ".join(map(str, L))
+        total = sum(L)
+        return f"{equation} = {total}"
+
 
 def main():
     pass
+
 
 if __name__ == "__main__":
     main()
