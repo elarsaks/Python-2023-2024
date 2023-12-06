8d0bbcf80268c608ec040968d4d456f52abf3c30
Elar Saks <elarsaks@gmail.com>
2023-12-06 16:01:38 +0200

    Do regex function for finding Ints.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e01_integers_in_brackets/src/integers_in_brackets.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e01_integers_in_brackets/src/integers_in_brackets.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
+import re
 
 
 def integers_in_brackets(s):
-    return []
+    # This regular expression looks for numbers enclosed in brackets
+    # It handles optional whitespace inside the brackets as well
+    pattern = r'\[\s*([+-]?\d+)\s*\]'
+    # Find all matches in the string
+    matches = re.findall(pattern, s)
+    # Convert all matches to integers
+    return [int(number) for number in matches]
+
 
 def main():
     pass
+
 
 if __name__ == "__main__":
     main()
