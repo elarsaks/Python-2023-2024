d0fdae32b7d3a5c8d8f652b801cec21dc18afe98
Elar Saks <elarsaks@gmail.com>
2023-12-07 11:34:21 +0200

    Start "word frequencies".

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e04_word_frequencies/src/word_frequencies.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e04_word_frequencies/src/word_frequencies.py
@@ -1,10 +1,39 @@
 #!/usr/bin/env python3
 
 def word_frequencies(filename):
-    return {}
+    # Dictionary to hold word frequencies
+    freqs = {}
 
+    # Define punctuation to be removed
+    punctuation = """!"#$%&'()*,-./:;?@[]_"""
+
+    # Read and process the file
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            # Split the line into words
+            words = line.split()
+
+            # Process each word
+            for word in words:
+                # Remove punctuation from the ends of the word
+                cleaned_word = word.strip(punctuation)
+
+                # Count the word
+                if cleaned_word:
+                    freqs[cleaned_word] = freqs.get(cleaned_word, 0) + 1
+
+    return freqs
+
+
+# Main function to test word_frequencies
 def main():
-    pass
+    filename = "src/alice.txt"
+    frequencies = word_frequencies(filename)
+
+    # Print word and its count per line
+    for word, count in sorted(frequencies.items(), key=lambda item: item[1], reverse=True):
+        print(f"{word}\t{count}")
+
 
 if __name__ == "__main__":
     main()
