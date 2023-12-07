d1a5390d796c3932d69e87c05d5da8246839bf26
Elar Saks <elarsaks@gmail.com>
2023-12-07 19:37:53 +0200

    Do the line count.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e06_file_count/src/file_count.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e06_file_count/src/file_count.py
@@ -1,12 +1,25 @@
-#!/usr/bin/env python3
-
 import sys
 
+
 def file_count(filename):
-    return (0, 0, 0)
+    lines_count = 0
+    words_count = 0
+    chars_count = 0
+
+    with open(filename, 'r') as file:
+        for line in file:
+            lines_count += 1
+            words_count += len(line.split())
+            chars_count += len(line)
+
+    return lines_count, words_count, chars_count
+
 
 def main():
-    pass
+    for filename in sys.argv[1:]:
+        line_count, word_count, char_count = file_count(filename)
+        print(f"{line_count}\t{word_count}\t{char_count}\t{filename}")
+
 
 if __name__ == "__main__":
     main()
