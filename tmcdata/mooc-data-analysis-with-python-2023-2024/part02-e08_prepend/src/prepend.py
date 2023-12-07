78b91717dacd788f276ad6595f400ce2c42b3ab6
Elar Saks <elarsaks@gmail.com>
2023-12-07 20:01:48 +0200

    Do prepend.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e08_prepend/src/prepend.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e08_prepend/src/prepend.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 
 class Prepend(object):
-    # Add the methods of the class here
+    def __init__(self, start):
+        self.start = start
+
+    def write(self, text):
+        print(self.start + text)
+
 
 def main():
+    p = Prepend("+++ ")
+    p.write("Hello")
     pass
+
 
 if __name__ == "__main__":
     main()
