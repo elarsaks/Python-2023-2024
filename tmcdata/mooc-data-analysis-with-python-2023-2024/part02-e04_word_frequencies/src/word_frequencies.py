adf88d0ed5e7f60ef36244284c6537088d8f37c7
Elar Saks <elarsaks@gmail.com>
2023-12-07 17:43:21 +0200

    Start the summary.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e04_word_frequencies/src/word_frequencies.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e04_word_frequencies/src/word_frequencies.py
@@ -1,37 +1,18 @@
 #!/usr/bin/env python3
 
 def word_frequencies(filename):
-    # Dictionary to hold word frequencies
-    freqs = {}
-
-    # Define punctuation to be removed
-    punctuation = """!"#$%&'()*,-./:;?@[]_"""
-
-    # Read and process the file
-    with open(filename, 'r', encoding='utf-8') as file:
-        for line in file:
-            # Split the line into words
-            words = line.split()
-
-            # Process each word
-            for word in words:
-                # Remove punctuation from the ends of the word
-                cleaned_word = word.strip(punctuation)
-
-                # Count the word
-                if cleaned_word:
-                    freqs[cleaned_word] = freqs.get(cleaned_word, 0) + 1
-
-    return freqs
+    result = {}
+    with open(filename) as in_file:
+        for w in in_file.read().split():
+            ws = w.strip("""!"#$%&'()*,-./:;?@[]_""")
+            if ws not in result:
+                result[ws] = 0
+            result[ws] += 1
+    return result
 
 
-# Main function to test word_frequencies
 def main():
-    filename = "src/alice.txt"
-    frequencies = word_frequencies(filename)
-
-    # Print word and its count per line
-    for word, count in sorted(frequencies.items(), key=lambda item: item[1], reverse=True):
+    for word, count in word_frequencies("src/alice.txt").items():
         print(f"{word}\t{count}")
 
 
--- tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e05_summary/src/summary.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e05_summary/src/summary.py
@@ -1,12 +1,52 @@
 #!/usr/bin/env python3
-
 import sys
+import math
+
 
 def summary(filename):
-    return (0,0,0)
+    # Open the file for reading
+    with open(filename, 'r') as file:
+        numbers = []  # List to store valid numbers from the file
+
+        # Iterate over each line in the file
+        for line in file:
+            try:
+                # Try to convert the line to a float
+                numbers.append(float(line))
+            except ValueError:
+                # If conversion fails, skip the line
+                continue
+
+        # If no valid numbers were found, return zeros
+        if len(numbers) == 0:
+            return (0, 0, 0)
+
+        # Calculate the sum of the numbers
+        total_sum = sum(numbers)
+
+        # Calculate the average (mean) of the numbers
+        average = total_sum / len(numbers)
+
+        # Calculate the variance, which is the average of the squared differences
+        # from the mean. This is a part of the standard deviation calculation.
+        variance = sum((x - average) ** 2 for x in numbers) / \
+            (len(numbers) - 1)
+
+        # Standard deviation is the square root of the variance. It represents
+        # a measure of the amount of variation or dispersion of a set of values.
+        stddev = math.sqrt(variance)
+
+        return (total_sum, average, stddev)
+
 
 def main():
-    pass
+    # Iterate over each filename passed as a command line argument
+    for filename in sys.argv[1:]:
+        total_sum, average, stddev = summary(filename)
+        # Print the results with six decimal precision
+        print(
+            f"File: {filename} Sum: {total_sum:.6f} Average: {average:.6f} Stddev: {stddev:.6f}")
+
 
 if __name__ == "__main__":
     main()
