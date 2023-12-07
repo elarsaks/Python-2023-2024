74791366a80e69f164cd29cbc2fde6c0c95f649d
Elar Saks <elarsaks@gmail.com>
2023-12-07 08:48:35 +0200

    Do "rec, green, blue".

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e03_red_green_blue/src/red_green_blue.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e03_red_green_blue/src/red_green_blue.py
@@ -2,12 +2,28 @@
 
 import re
 
+
 def red_green_blue(filename="src/rgb.txt"):
-    return []
+    with open(filename, 'r') as file:
+        # Skip the first line
+        next(file)
+
+        result = []
+        for line in file:
+            # Find all non-whitespace sequences in the line
+            parts = re.findall(r'\S+', line.strip())
+
+            # Reformat to have fields separated by a single tab character
+            # The first three parts are RGB values, the rest is the color name
+            formatted_line = '\t'.join(parts[:3]) + '\t' + ' '.join(parts[3:])
+            result.append(formatted_line)
+
+        return result
 
 
 def main():
     pass
+
 
 if __name__ == "__main__":
     main()
