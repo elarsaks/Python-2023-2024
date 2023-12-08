00308e596bc3ec5acb0f33fad4269d9130eab804
Elar Saks <elarsaks@gmail.com>
2023-12-08 09:01:22 +0200

    Extract numbers.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e10_extract_numbers/src/extract_numbers.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e10_extract_numbers/src/extract_numbers.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python3
 
 def extract_numbers(s):
-    return []
+    numbers = []
+    for word in s.split():
+        try:
+            # Try converting to int first
+            num = int(word)
+        except ValueError:
+            try:
+                # If int conversion fails, try converting to float
+                num = float(word)
+            except ValueError:
+                # If both conversions fail, continue to the next word
+                continue
+        numbers.append(num)
+    return numbers
+
 
 def main():
     print(extract_numbers("abd 123 1.2 test 13.2 -1"))
+
 
 if __name__ == "__main__":
     main()
