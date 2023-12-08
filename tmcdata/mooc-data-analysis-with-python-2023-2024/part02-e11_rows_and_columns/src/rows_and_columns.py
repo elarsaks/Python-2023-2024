8d539d6a23368ca1fd71ac64e081b65042c3f026
Elar Saks <elarsaks@gmail.com>
2023-12-08 13:46:24 +0200

    Start with Numpy - do rows & columns.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e11_rows_and_columns/src/rows_and_columns.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e11_rows_and_columns/src/rows_and_columns.py
@@ -2,18 +2,24 @@
 
 import numpy as np
 
+
 def get_rows(a):
-    return []
+    """Return the list of rows of the given 2D array."""
+    return [row for row in a]
+
 
 def get_columns(a):
-    return []
+    """Return the list of columns of the given 2D array."""
+    return [a[:, i] for i in range(a.shape[1])]
+
 
 def main():
     np.random.seed(0)
-    a=np.random.randint(0,10, (4,4))
+    a = np.random.randint(0, 10, (4, 4))
     print("a:", a)
     print("Rows:", get_rows(a))
     print("Columns:", get_columns(a))
+
 
 if __name__ == "__main__":
     main()
