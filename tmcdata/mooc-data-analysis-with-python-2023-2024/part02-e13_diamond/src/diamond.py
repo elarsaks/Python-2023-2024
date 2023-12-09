bc45cc82d5ed8216eb14eb67d0606a1c759e2eaf
Elar Saks <elarsaks@gmail.com>
2023-12-09 21:50:09 +0200

    Do the diamond.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e13_diamond/src/diamond.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e13_diamond/src/diamond.py
@@ -2,11 +2,30 @@
 
 import numpy as np
 
+
 def diamond(n):
-    return np.array([])
+    # Create an identity matrix of size n
+    identity_matrix = np.eye(n, dtype=int)
+
+    # Create the upper half of the diamond
+    upper_half = np.concatenate(
+        (np.flip(identity_matrix, axis=1), identity_matrix[:, 1:]), axis=1)
+
+    # Create the lower half of the diamond
+    lower_half = np.concatenate(
+        (identity_matrix, np.flip(identity_matrix, axis=1)[:, 1:]), axis=1)
+
+    # Concatenate the upper and lower halves
+    diamond_shape = np.concatenate((upper_half, lower_half[1:]), axis=0)
+
+    return diamond_shape
+
 
 def main():
-    pass
+    n = 5  # Example side length
+    diamond_shape = diamond(n)
+    print(diamond_shape)
+
 
 if __name__ == "__main__":
     main()
