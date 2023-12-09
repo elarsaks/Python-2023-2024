659ffd13b3f467a06b1855d0ca205ad9c69f68ca
Elar Saks <elarsaks@gmail.com>
2023-12-09 23:15:55 +0200

    Do vector lengths.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e14_vector_lengths/src/vector_lengths.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e14_vector_lengths/src/vector_lengths.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 import numpy as np
-#import scipy.linalg
+
 
 def vector_lengths(a):
-    return np.array([])
+    # Calculate the squared values of all elements
+    squared_elements = np.square(a)
+
+    # Sum the squares along each row (axis=1)
+    sum_of_squares = np.sum(squared_elements, axis=1)
+
+    # Take the square root of the sum of squares
+    lengths = np.sqrt(sum_of_squares)
+
+    return lengths
+
 
 def main():
-    pass
+    a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    print("Input array:\n", a)
+    print("Lengths of vectors:", vector_lengths(a))
+
 
 if __name__ == "__main__":
     main()
