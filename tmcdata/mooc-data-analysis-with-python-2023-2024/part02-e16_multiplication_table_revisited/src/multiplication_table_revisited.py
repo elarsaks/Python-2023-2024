acef89aa99291fd3e7ea5f11c06c5dac48bf1567
Elar Saks <elarsaks@gmail.com>
2023-12-09 23:27:18 +0200

    Do multiplication table: Finnish part 2.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e16_multiplication_table_revisited/src/multiplication_table_revisited.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e16_multiplication_table_revisited/src/multiplication_table_revisited.py
@@ -2,11 +2,23 @@
 
 import numpy as np
 
+
 def multiplication_table(n):
-    return np.array([])
+    # Create an array of values from 0 to n-1
+    values = np.arange(n)
+
+    # Reshape values to a column vector for broadcasting
+    column_values = values.reshape(n, 1)
+
+    # Use broadcasting to multiply column vector with row vector (values)
+    table = column_values * values
+
+    return table
+
 
 def main():
     print(multiplication_table(4))
+
 
 if __name__ == "__main__":
     main()
