fa8da91bc3bf5508a0194d1d31b092154c8e47b4
Elar Saks <elarsaks@gmail.com>
2023-12-09 21:10:44 +0200

    Do vectors task.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e12_row_and_column_vectors/src/row_and_column_vectors.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part02-e12_row_and_column_vectors/src/row_and_column_vectors.py
@@ -2,18 +2,35 @@
 
 import numpy as np
 
+
 def get_row_vectors(a):
-    return []
+    # Create a list of row vectors from the array 'a'
+    # Each row vector is extracted with a new axis to make its shape (1, m)
+    return [a[i, np.newaxis, :] for i in range(a.shape[0])]
+
 
 def get_column_vectors(a):
-    return []
+    # Create a list of column vectors from the array 'a'
+    # Each column vector is extracted with a new axis to make its shape (n, 1)
+    return [a[:, i, np.newaxis] for i in range(a.shape[1])]
+
 
 def main():
+    # Set a seed for the random number generator for reproducibility
     np.random.seed(0)
-    a=np.random.randint(0,10, (4,4))
+
+    # Generate a 4x4 array of random integers between 0 and 9
+    a = np.random.randint(0, 10, (4, 4))
+
+    # Print the generated array
     print("a:", a)
+
+    # Print the row vectors of the array
     print("Row vectors:", get_row_vectors(a))
+
+    # Print the column vectors of the array
     print("Column vectors:", get_column_vectors(a))
+
 
 if __name__ == "__main__":
     main()
