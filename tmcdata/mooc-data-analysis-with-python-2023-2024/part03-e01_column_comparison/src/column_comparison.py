4f06c71973aea6437e4188c7aafede76c8770f20
Elar Saks <elarsaks@gmail.com>
2023-12-11 14:39:32 +0200

    Do the column comparison.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part03-e01_column_comparison/src/column_comparison.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part03-e01_column_comparison/src/column_comparison.py
@@ -1,12 +1,23 @@
-#!/usr/bin/env python3
-
 import numpy as np
 
+
 def column_comparison(a):
-    return np.array([])
-    
+    # Boolean indexing to filter rows where the second column value is greater than the second-to-last column value
+    return a[a[:, 1] > a[:, -2]]
+
+
 def main():
-    pass
+    # Example array
+    arr = np.array([[8, 9, 3, 8, 8],
+                    [0, 5, 3, 9, 9],
+                    [5, 7, 6, 0, 4],
+                    [7, 8, 1, 6, 2],
+                    [2, 1, 3, 5, 8]])
+
+    # Testing the function
+    result = column_comparison(arr)
+    print(result)
+
 
 if __name__ == "__main__":
     main()
