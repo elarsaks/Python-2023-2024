c463ff1d55e00de7f8b8609f0fa5b48be89f67fb
Elar Saks <elarsaks@gmail.com>
2023-12-12 07:16:42 +0200

    Do first half, second half.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part03-e02_first_half_second_half/src/first_half_second_half.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part03-e02_first_half_second_half/src/first_half_second_half.py
@@ -2,11 +2,30 @@
 
 import numpy as np
 
-def first_half_second_half(a):
-    return np.array([])
+
+def first_half_second_half(arr):
+    # Check if the array has an even number of columns
+    n, columns = arr.shape
+    if columns % 2 != 0:
+        raise ValueError("The number of columns should be even.")
+
+    m = columns // 2
+
+    # Calculate the sum of the first half and the second half of each row
+    sum_first_half = np.sum(arr[:, :m], axis=1)
+    sum_second_half = np.sum(arr[:, m:], axis=1)
+
+    # Select rows where the sum of the first half is greater than the sum of the second half
+    result = arr[sum_first_half > sum_second_half]
+
+    return result
+
 
 def main():
-    pass
+    arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 9]])
+    result = first_half_second_half(arr)
+    print(result)
+
 
 if __name__ == "__main__":
     main()
