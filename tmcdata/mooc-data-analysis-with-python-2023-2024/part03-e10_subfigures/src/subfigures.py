8ab78eec83d12c554b4e18da9b1b7331e308d7db
Elar Saks <elarsaks@gmail.com>
2023-12-12 23:51:43 +0200

    Do the subplots task.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part03-e10_subfigures/src/subfigures.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part03-e10_subfigures/src/subfigures.py
@@ -1,13 +1,39 @@
-#!/usr/bin/env python3
-
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 def subfigures(a):
-    pass
+    # Create a figure with two subplots (axes)
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+
+    # Plot on the first subplot (ax1)
+    ax1.plot(a[:, 0], a[:, 1])
+    ax1.set_title('Line Plot')
+    ax1.set_xlabel('X-axis')
+    ax1.set_ylabel('Y-axis')
+
+    # Scatter plot on the second subplot (ax2)
+    scatter = ax2.scatter(a[:, 0], a[:, 1], c=a[:, 2], s=a[:, 3])
+    ax2.set_title('Scatter Plot')
+    ax2.set_xlabel('X-axis')
+    ax2.set_ylabel('Y-axis')
+
+    # Show the plot
+    plt.show()
+
 
 def main():
-    pass
+    # Test data
+    # Columns: x, y, color, size
+    data = np.array([
+        [1, 2, 3, 100],
+        [2, 3, 4, 200],
+        [3, 4, 5, 300],
+        [4, 5, 6, 400]
+    ])
+
+    subfigures(data)
+
 
 if __name__ == "__main__":
     main()
