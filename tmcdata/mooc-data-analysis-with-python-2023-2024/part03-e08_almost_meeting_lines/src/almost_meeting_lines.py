34140267228f460b0c8ee7492a6b7f5a8474021f
Elar Saks <elarsaks@gmail.com>
2023-12-12 16:25:31 +0200

    3.7 & 3.8 failing.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part03-e08_almost_meeting_lines/src/almost_meeting_lines.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part03-e08_almost_meeting_lines/src/almost_meeting_lines.py
@@ -2,45 +2,41 @@
 
 import numpy as np
 
+
 def almost_meeting_lines(a1, b1, a2, b2):
-    return []
+    A = np.array([[a1, b1], [a2, b2]])
+    B = np.array([1, 1])
+
+    # Check if lines are parallel (determinant is zero or close to zero)
+    if np.isclose(np.linalg.det(A), 0):
+        # Use least squares to find the closest point
+        result = np.linalg.lstsq(A, B, rcond=None)
+        point = result[0]
+        return (point[0], point[1]), False
+    else:
+        # Lines are not parallel, solve normally
+        point = np.linalg.solve(A, B)
+        return (point[0], point[1]), True
+
 
 def main():
-    a1=1
-    b1=2
-    a2=-1
-    b2=0
-
+    a1, b1, a2, b2 = 1, 2, -1, 0
     (x, y), exact = almost_meeting_lines(a1, b1, a2, b2)
-    if exact:
-        print(f"Lines meet at x={x} and y={y}")
+    print(x, y, exact)
 
-    a1=a2=1
-    b1=2
-    b2=-2
-    (x, y), exact = almost_meeting_lines(a1, b1, a1, b2)
-    if exact:
-        print(f"Lines meet at x={x} and y={y}")
-    else:
-        print(f"Closest point at x={x} and y={y}")
-
-    a1=1
-    b1=2
-    (x, y), exact = almost_meeting_lines(a1, b1, a1, b1)
-    if exact:
-        print(f"Lines meet at x={x} and y={y}")
-    else:
-        print(f"Closest point at x={x} and y={y}")
-
-    a1=1
-    b1=2
-    a2=1
-    b2=1
+    # Additional test cases
+    a1, b1, a2, b2 = 1, 2, 1, -2
     (x, y), exact = almost_meeting_lines(a1, b1, a2, b2)
-    if exact:
-        print(f"Lines meet at x={x} and y={y}")
-    else:
-        print(f"Closest point at x={x} and y={y}")
+    print(f"Closest point at x={x} and y={y}, exact={exact}")
+
+    a1, b1, a2, b2 = 1, 2, 1, 2
+    (x, y), exact = almost_meeting_lines(a1, b1, a2, b2)
+    print(f"Closest point at x={x} and y={y}, exact={exact}")
+
+    a1, b1, a2, b2 = 1, 2, 1, 1
+    (x, y), exact = almost_meeting_lines(a1, b1, a2, b2)
+    print(f"Closest point at x={x} and y={y}, exact={exact}")
+
 
 if __name__ == "__main__":
     main()
