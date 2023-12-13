1ecc882e142340423b9a39e2813b9c91dad16b61
Elar Saks <elarsaks@gmail.com>
2023-12-13 16:30:59 +0200

    Practice series.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part03-e14_operations_on_series/src/operations_on_series.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part03-e14_operations_on_series/src/operations_on_series.py
@@ -1,14 +1,63 @@
 #!/usr/bin/env python3
 
+import pandas as pd
+
 
 def create_series(L1, L2):
-    return (None, None)
-    
+    """
+    Create two Series from two lists with indices 'a', 'b', and 'c'.
+
+    Parameters:
+    L1 (list): A list of numbers for the first Series.
+    L2 (list): A list of numbers for the second Series.
+
+    Returns:
+    tuple: A tuple containing the two created Series.
+    """
+    if len(L1) != 3 or len(L2) != 3:
+        raise ValueError("Both lists should have length 3.")
+
+    indices = ['a', 'b', 'c']
+    s1 = pd.Series(L1, index=indices)
+    s2 = pd.Series(L2, index=indices)
+    return s1, s2
+
+
 def modify_series(s1, s2):
-    return (None, None)
-    
+    """
+    Modify the first Series by adding a new value with index 'd', which is the same as the value in the second Series with index 'b'.
+    Then delete the element with index 'b' from the second Series.
+
+    Parameters:
+    s1 (pd.Series): The first Series.
+    s2 (pd.Series): The second Series.
+
+    Returns:
+    tuple: A tuple containing the modified Series.
+    """
+    s1['d'] = s2['b']
+    del s2['b']
+    return s1, s2
+
+
 def main():
-    return
-    
+    # Test the functions
+    L1 = [1, 2, 3]
+    L2 = [4, 5, 6]
+
+    s1, s2 = create_series(L1, L2)
+    print("Original Series:")
+    print("s1:\n", s1)
+    print("s2:\n", s2)
+
+    s1, s2 = modify_series(s1, s2)
+    print("\nModified Series:")
+    print("s1:\n", s1)
+    print("s2:\n", s2)
+
+    print("\nSum of Series:")
+    print(s1 + s2)
+
+
 if __name__ == "__main__":
     main()
