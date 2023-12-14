cc49c3140b9a1990c4a81189affa700580820c0c
Elar Saks <elarsaks@gmail.com>
2023-12-14 20:53:37 +0200

    Use 'loc' function.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part04-e07_subsetting_with_loc/src/subsetting_with_loc.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part04-e07_subsetting_with_loc/src/subsetting_with_loc.py
@@ -2,11 +2,21 @@
 
 import pandas as pd
 
+
 def subsetting_with_loc():
-    return None
+    file_path = 'src/municipal.tsv'
+    df = pd.read_csv(file_path, sep='\t', index_col=0)
+
+    # Using loc to select rows from 'Akaa' to 'Äänekoski' and the specified columns
+    result = df.loc['Akaa':'Äänekoski', [
+        'Population', 'Share of Swedish-speakers of the population, %', 'Share of foreign citizens of the population, %']]
+    return result
+
 
 def main():
+    print(subsetting_with_loc())
     return
+
 
 if __name__ == "__main__":
     main()
