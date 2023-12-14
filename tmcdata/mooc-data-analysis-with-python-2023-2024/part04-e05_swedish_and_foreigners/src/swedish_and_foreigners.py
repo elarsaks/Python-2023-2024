9821de14c5174f42f61c2a0451b2a8c9a86eccec
Elar Saks <elarsaks@gmail.com>
2023-12-14 20:24:48 +0200

    Do Finnish Svedish data filtering.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part04-e05_swedish_and_foreigners/src/swedish_and_foreigners.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part04-e05_swedish_and_foreigners/src/swedish_and_foreigners.py
@@ -2,11 +2,30 @@
 
 import pandas as pd
 
+
 def swedish_and_foreigners():
-    return None
+
+    # Read the dataset
+    df = pd.read_csv('src/municipal.tsv', sep='\t', index_col=0)
+
+    # Get only the municipalities
+    municipalities = df.loc['Akaa':'Äänekoski']
+
+    # Filter rows where both Swedish-speaking and foreigners are above 5%
+    filtered = municipalities[(municipalities['Share of Swedish-speakers of the population, %'] > 5) &
+                              (municipalities['Share of foreign citizens of the population, %'] > 5)]
+
+    # Select only the required columns
+    result = filtered[['Population', 'Share of Swedish-speakers of the population, %',
+                       'Share of foreign citizens of the population, %']]
+
+    return result
+
 
 def main():
+    print(swedish_and_foreigners())
     return
+
 
 if __name__ == "__main__":
     main()
