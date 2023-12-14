066ad6ccf55ec26ec6a22a35b5f23d050f3a5ba1
Elar Saks <elarsaks@gmail.com>
2023-12-14 20:11:56 +0200

    Do Finland municipalities.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part04-e04_municipalities_of_finland/src/municipalities_of_finland.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part04-e04_municipalities_of_finland/src/municipalities_of_finland.py
@@ -2,11 +2,31 @@
 
 import pandas as pd
 
+
 def municipalities_of_finland():
-    return None
-    
+    file_path = 'src/municipal.tsv'
+
+    # Load the dataset with the first column as the index
+    df = pd.read_csv(file_path, sep='\t', index_col=0)
+
+    # Get only the municipalities
+    mun_df = df.loc['Akaa':'Äänekoski']
+
+    # Print the shape of the DataFrame
+    print(f"Shape: {mun_df.shape[0]},{mun_df.shape[1]}")
+
+    # Print the column names
+    print("Columns:")
+    for col in mun_df.columns:
+        print(col)
+
+    return mun_df
+
+
 def main():
+    municipalities_of_finland()
     return
-    
+
+
 if __name__ == "__main__":
     main()
