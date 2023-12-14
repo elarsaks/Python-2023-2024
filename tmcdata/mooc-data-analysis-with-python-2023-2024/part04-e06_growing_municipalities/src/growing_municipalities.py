8e1aab6b5739e0264a1e01b368f5bfca92f4dea5
Elar Saks <elarsaks@gmail.com>
2023-12-14 20:44:23 +0200

    Calculate the percentage of growing municipalities.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part04-e06_growing_municipalities/src/growing_municipalities.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part04-e06_growing_municipalities/src/growing_municipalities.py
@@ -2,11 +2,35 @@
 
 import pandas as pd
 
+import pandas as pd
+
+
 def growing_municipalities(df):
-    0.0
+    # Count the number of municipalities with increasing population
+    growing_count = df[df['Population change from the previous year, %'] > 0].shape[0]
+
+    # Calculate the proportion as a decimal
+    proportion = (growing_count / df.shape[0])
+
+    return proportion
+
 
 def main():
-    return
+    file_path = 'src/municipal.tsv'  # Replace with your actual file path
+    df = pd.read_csv(file_path, sep='\t', index_col=0)
 
+    # Get only the municipalities
+    municipalities = df.loc['Akaa':'Äänekoski']
+
+    # Test the function with the subset of municipalities
+    proportion = growing_municipalities(municipalities)
+
+    # Format the proportion as a percentage with one decimal place
+    formatted_proportion = f"{proportion * 100:.1f}"
+
+    print(f"Proportion of growing municipalities: {formatted_proportion}%")
+
+
+# Run the main function
 if __name__ == "__main__":
     main()
