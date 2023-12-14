3a2e285f54b3fcef3d76c783fa740dfe9b82b7b8
Elar Saks <elarsaks@gmail.com>
2023-12-14 19:08:51 +0200

    Do "power of series".

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part04-e02_powers_of_series/src/powers_of_series.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part04-e02_powers_of_series/src/powers_of_series.py
@@ -2,11 +2,21 @@
 
 import pandas as pd
 
+
 def powers_of_series(s, k):
-    return None
-    
+    # Create a DataFrame from the Series
+    df = pd.DataFrame({i: s**i for i in range(1, k+1)})
+
+    return df
+
+
 def main():
+    # Test the function
+    s = pd.Series([1, 2, 3, 4], index=list("abcd"))
+    print(powers_of_series(s, 3))
+
     return
-    
+
+
 if __name__ == "__main__":
     main()
