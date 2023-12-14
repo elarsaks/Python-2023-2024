029b6f4cd6b350e19cab7d4914140c0700dffd1e
Elar Saks <elarsaks@gmail.com>
2023-12-14 19:25:04 +0200

    Create dataframe from the data in file.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part04-e03_municipal_information/src/municipal_information.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part04-e03_municipal_information/src/municipal_information.py
@@ -2,9 +2,23 @@
 
 import pandas as pd
 
+
 def main():
-    return
+    file_path = 'src/municipal.tsv'
+
+    # Load the dataset
+    # Use sep='\t' for tab-separated values
+    df = pd.read_csv(file_path, sep='\t')
+
+    # Print the shape of the DataFrame
+    print(f"Shape: {df.shape[0]},{df.shape[1]}")
+
+    # Print the column names
+    print("Columns:")
+    for col in df.columns:
+        print(col)
 
 
+# Run the main function
 if __name__ == "__main__":
     main()
