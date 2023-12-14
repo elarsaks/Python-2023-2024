e7e259dae1d82bc790345cac3f19daf8f36221ab
Elar Saks <elarsaks@gmail.com>
2023-12-14 19:03:34 +0200

    Create pandas dataframe.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part04-e01_cities/src/cities.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part04-e01_cities/src/cities.py
@@ -2,11 +2,27 @@
 
 import pandas as pd
 
+
 def cities():
-    return None
-    
+    # Define the data
+    data = {
+        'Population': [643272, 279044, 231853, 223027, 201810],
+        'Total area': [715.48, 528.03, 689.59, 240.35, 3817.52]
+    }
+
+    # Define the index (city names)
+    index = ['Helsinki', 'Espoo', 'Tampere', 'Vantaa', 'Oulu']
+
+    # Create the DataFrame
+    df = pd.DataFrame(data, index=index)
+
+    return df
+
+
 def main():
+    print(cities())
     return
-    
+
+
 if __name__ == "__main__":
     main()
