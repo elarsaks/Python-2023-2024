d55d23dd419c52b63f6b2a5cf7f4019ff79acefa
Elar Saks <elarsaks@gmail.com>
2023-12-15 12:51:02 +0200

    DO the average temp.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part04-e10_average_temperature/src/average_temperature.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part04-e10_average_temperature/src/average_temperature.py
@@ -2,11 +2,24 @@
 
 import pandas as pd
 
+
 def average_temperature():
-    return 0.0
+    # Read the DataFrame from the specified file
+    df = pd.read_csv('src/kumpula-weather-2017.csv')
+
+    # Filter the DataFrame for the month of July (7)
+    july_df = df[df['m'] == 7]
+
+    # Calculate the average temperature
+    avg_temp = july_df['Air temperature (degC)'].mean()
+
+    return avg_temp
+
 
 def main():
-    return
+    avg_temp_july = average_temperature()
+    print(f"Average temperature in July: {avg_temp_july:.1f}")
+
 
 if __name__ == "__main__":
     main()
