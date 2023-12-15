8809ccde323c028405ccd8f10be5d8c41f547a7b
Elar Saks <elarsaks@gmail.com>
2023-12-15 18:01:33 +0200

    DO the cyclist dataset.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part04-e12_cyclists/src/cyclists.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part04-e12_cyclists/src/cyclists.py
@@ -2,12 +2,25 @@
 
 import pandas as pd
 
+
 def cyclists():
-    return None
+    # Load the dataset
+    df = pd.read_csv("src/Helsingin_pyorailijamaarat.csv", delimiter=';')
+
+    # Remove empty rows at the end of the dataset
+    df.dropna(how='all', inplace=True)
+
+    # Drop columns that contain only missing values
+    df.dropna(axis=1, how='all', inplace=True)
+
+    return df
 
 
 def main():
-    return
-    
+    # Call the function and print the result
+    cleaned_data = cyclists()
+    print(cleaned_data)
+
+
 if __name__ == "__main__":
     main()
