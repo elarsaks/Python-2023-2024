e86fc83b7b73a47ad557a9681781e2ea8d345f22
Elar Saks <elarsaks@gmail.com>
2023-12-15 18:46:07 +0200

    Do the missing values.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part04-e13_missing_value_types/src/missing_value_types.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part04-e13_missing_value_types/src/missing_value_types.py
@@ -3,11 +3,22 @@
 import pandas as pd
 import numpy as np
 
-def missing_value_types():
-    return None
-               
-def main():
-    return
 
-if __name__ == "__main__":
-    main()
+def missing_value_types():
+    # Create a DataFrame with the given data
+    data = {
+        "State": ["United Kingdom", "Finland", "USA", "Sweden", "Germany", "Russia"],
+        "Year of independence": [np.nan, 1917, 1776, 1523, np.nan, 1992],
+        "President": [None, "Niinistö", "Trump", None, "Steinmeier", "Putin"]
+    }
+    df = pd.DataFrame(data)
+
+    # Set the 'State' column as the index
+    df.set_index("State", inplace=True)
+
+    return df
+
+
+# Test the function
+df = missing_value_types()
+print(df)
