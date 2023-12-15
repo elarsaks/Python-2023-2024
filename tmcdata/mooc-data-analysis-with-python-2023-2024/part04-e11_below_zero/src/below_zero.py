22ae16f06e9c46036002f553df2309954b7a3fc1
Elar Saks <elarsaks@gmail.com>
2023-12-15 12:54:55 +0200

    Do below zero.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part04-e11_below_zero/src/below_zero.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part04-e11_below_zero/src/below_zero.py
@@ -2,11 +2,24 @@
 
 import pandas as pd
 
+
 def below_zero():
-    return 0
+    # Read the DataFrame from the specified file
+    df = pd.read_csv('src/kumpula-weather-2017.csv')
+
+    # Filter the DataFrame for days with temperatures below zero
+    below_zero_df = df[df['Air temperature (degC)'] < 0]
+
+    # Count the number of days
+    num_days_below_zero = below_zero_df.shape[0]
+
+    return num_days_below_zero
+
 
 def main():
-    return
-    
+    days_below_zero = below_zero()
+    print(f"Number of days below zero: {days_below_zero}")
+
+
 if __name__ == "__main__":
     main()
