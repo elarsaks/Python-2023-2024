1d67b37b16dc9ad922cabbf0344ce8faf80d5398
Elar Saks <elarsaks@gmail.com>
2023-12-15 11:56:43 +0200

    DO subsetting by position.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part04-e08_subsetting_by_positions/src/subsetting_by_positions.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part04-e08_subsetting_by_positions/src/subsetting_by_positions.py
@@ -2,11 +2,23 @@
 
 import pandas as pd
 
+
 def subsetting_by_positions():
-    return pd.DataFrame()
+    # Read the TSV dataset, specifying the delimiter as '\t'
+    df = pd.read_csv('src/UK-top40-1964-1-2.tsv', delimiter='\t')
+
+    # Subset the DataFrame using iloc to get the top 10 entries and the columns for Title and Artist
+    # Adjust the column indices to match the positions of 'Title' and 'Artist'
+    # 'Title' is at index 2 and 'Artist' is at index 3
+    result_df = df.iloc[0:10, [2, 3]]
+
+    return result_df
+
 
 def main():
-    return
+    result = subsetting_by_positions()
+    print(result)
+
 
 if __name__ == "__main__":
     main()
