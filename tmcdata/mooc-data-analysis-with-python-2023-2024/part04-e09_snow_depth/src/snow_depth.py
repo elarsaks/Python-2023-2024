4b1fdc2f842a748340aa9b24ded77d411a60d722
Elar Saks <elarsaks@gmail.com>
2023-12-15 12:29:02 +0200

    Do the snow depth.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part04-e09_snow_depth/src/snow_depth.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part04-e09_snow_depth/src/snow_depth.py
@@ -2,11 +2,26 @@
 
 import pandas as pd
 
+import pandas as pd
+
+
 def snow_depth():
-    return 0.0
+    # Read the DataFrame from the specified file
+    df = pd.read_csv('src/kumpula-weather-2017.csv')
+
+    # Filter the DataFrame for the year 2017
+    df_2017 = df[df['Year'] == 2017]
+
+    # Find the maximum snow depth
+    max_snow = df_2017['Snow depth (cm)'].max()
+
+    return max_snow
+
 
 def main():
-    return
+    max_snow_depth = snow_depth()
+    print(f"Max snow depth: {max_snow_depth:.1f}")
+
 
 if __name__ == "__main__":
     main()
