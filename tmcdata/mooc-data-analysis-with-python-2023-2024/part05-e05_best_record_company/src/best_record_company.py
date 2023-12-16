297de4c48637396be12ae00174275bbc87899e7a
Elar Saks <elarsaks@gmail.com>
2023-12-16 22:07:39 +0200

    Do the best record company.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part05-e05_best_record_company/src/best_record_company.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part05-e05_best_record_company/src/best_record_company.py
@@ -2,12 +2,28 @@
 
 import pandas as pd
 
+
 def best_record_company():
-    return None
+    # Load the data
+    df = pd.read_csv('src/UK-top40-1964-1-2.tsv', delimiter='\t')
+
+    # Group by record company and sum the weeks on chart
+    company_woc = df.groupby('Publisher')['WoC'].sum()
+
+    # Identify the best record company
+    best_company = company_woc.idxmax()
+
+    # Filter the DataFrame for the best record company
+    best_company_singles = df[df['Publisher'] == best_company]
+
+    return best_company_singles
+
 
 def main():
+    result = best_record_company()
+    print(result)
     return
-    
+
 
 if __name__ == "__main__":
     main()
--- tmcdata/mooc-data-analysis-with-python-2023-2024/part05-e06_suicide_fractions/src/suicide_fractions.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part05-e06_suicide_fractions/src/suicide_fractions.py
@@ -1,12 +1,20 @@
-#!/usr/bin/env python3
-
 import pandas as pd
 
-def suicide_fractions():
-    return pd.Series()
 
-def main():
-    return
+def suicide_fractions(filepath):
+    # Load the data
+    df = pd.read_csv(filepath)
 
-if __name__ == "__main__":
-    main()
+    # Calculate the fraction of suicides for each row
+    df['suicide_fraction'] = df['suicides_no'] / df['population']
+
+    # Group by country and calculate the mean suicide fraction
+    country_means = df.groupby('country')['suicide_fraction'].mean()
+
+    return country_means
+
+
+# Example usage
+filepath = 'src/suicide_data.csv'  # Replace with your actual file path
+result = suicide_fractions(filepath)
+print(result)
