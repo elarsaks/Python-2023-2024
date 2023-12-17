6f096e30a13f4952833f749eb584c0ed3cec9378
Elar Saks <elarsaks@gmail.com>
2023-12-17 20:05:20 +0200

    Coefficient of determination

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part05-e12_coefficient_of_determination/src/coefficient_of_determination.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part05-e12_coefficient_of_determination/src/coefficient_of_determination.py
@@ -1,13 +1,44 @@
 #!/usr/bin/env python3
 
 import pandas as pd
-from sklearn import linear_model
+from sklearn.linear_model import LinearRegression
 
 
 def coefficient_of_determination():
-    return []
-    
+    # Read the data
+    df = pd.read_csv('src/mystery_data.tsv', sep='\t')
+
+    # Split the data into features and response
+    X = df.iloc[:, :-1]  # All rows, all columns except the last one
+    y = df.iloc[:, -1]   # All rows, only the last column
+
+    # Initialize the linear regression model
+    model = LinearRegression()
+
+    # Fit the model with all features and calculate R2-score
+    model.fit(X, y)
+    r2_scores = [model.score(X, y)]
+
+    # Calculate R2-score for each feature
+    for i in range(X.shape[1]):
+        Xi = X.iloc[:, i].values.reshape(-1, 1)
+        model.fit(Xi, y)
+        r2_scores.append(model.score(Xi, y))
+
+    return r2_scores
+
+
 def main():
+    r2_scores = coefficient_of_determination()
+
+    # Print the R2-scores
+    features = ['X', 'X1', 'X2', 'X3', 'X4', 'X5']
+    for feature, score in zip(features, r2_scores):
+        print(f"R2-score with feature(s) {feature}: {score}")
+
+    # Experiment with fitting the intercept and without fitting the intercept
+    # Add your experiment code here if needed
+
 
 if __name__ == "__main__":
     main()
--- tmcdata/mooc-data-analysis-with-python-2023-2024/part05-e13_cycling_weather_continues/src/cycling_weather_continues.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part05-e13_cycling_weather_continues/src/cycling_weather_continues.py
@@ -1,12 +1,55 @@
-#!/usr/bin/env python3
-
+import pandas as pd
+from sklearn.linear_model import LinearRegression
 
 
 def cycling_weather_continues(station):
-    return ((0.0, 0.0, 0.0), 0.0)
-    
+    # Read and preprocess weather data
+    weather = pd.read_csv('src/kumpula-weather-2017.csv')
+    weather = weather.dropna(subset=['Air temperature (degC)'])
+
+    # Read cycling data
+    cycling = pd.read_csv('src/Helsingin_pyorailijamaarat.csv', sep=';')
+    cycling = cycling.dropna(how='all').dropna(axis=1, how='all')
+
+    # Remove weekday abbreviations from the date strings
+    cycling['Päivämäärä'] = cycling['Päivämäärä'].str.replace(
+        r'^\D{2}\s', '', regex=True)
+
+    # Convert 'Päivämäärä' to datetime
+    cycling['Date'] = pd.to_datetime(
+        cycling['Päivämäärä'], format='%d %b %Y %H:%M')
+
+    # Merge datasets
+    weather['Day'] = pd.to_datetime(weather['Year'].astype(
+        str) + '-' + weather['m'].astype(str) + '-' + weather['d'].astype(str))
+    data = pd.merge(cycling, weather, left_on='Day', right_on='Day')
+
+    # Forward fill missing values
+    data = data.fillna(method='ffill')
+
+    # Linear regression
+    X = data[[
+        'Precipitation amount (mm)', 'Snow depth (cm)', 'Air temperature (degC)']]
+    y = data[station]
+    model = LinearRegression(fit_intercept=True)
+    model.fit(X, y)
+    coeffs = model.coef_
+    score = model.score(X, y)
+
+    return coeffs, score
+
+
 def main():
-    return
+    station = 'Baana'
+    coeffs, score = cycling_weather_continues(station)
+    print(f"Measuring station: {station}")
+    print(
+        f"Regression coefficient for variable 'precipitation': {coeffs[0]:.1f}")
+    print(f"Regression coefficient for variable 'snow depth': {coeffs[1]:.1f}")
+    print(
+        f"Regression coefficient for variable 'temperature': {coeffs[2]:.1f}")
+    print(f"Score: {score:.2f}")
+
 
 if __name__ == "__main__":
     main()
