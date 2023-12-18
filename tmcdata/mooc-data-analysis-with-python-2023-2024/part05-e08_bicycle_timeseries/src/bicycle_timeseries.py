9a170b3c6b74785b1d9ac73b482d4106ad470a9d
Elar Saks <elarsaks@gmail.com>
2023-12-18 21:02:43 +0200

    Do the Blob classification.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part05-e08_bicycle_timeseries/src/bicycle_timeseries.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part05-e08_bicycle_timeseries/src/bicycle_timeseries.py
@@ -1,13 +1,36 @@
-#!/usr/bin/env python3
-
 import pandas as pd
 
 
 def bicycle_timeseries():
-    return pd.DataFrame()
+    # Read the data
+    df = pd.read_csv('src/Helsingin_pyorailijamaarat.csv', sep=';')
+
+    # Drop columns that are entirely NaN
+    df = df.dropna(axis=1, how='all')
+
+    # Check the format of the dates in 'Päivämäärä'
+    print("Sample dates:", df['Päivämäärä'].head())
+
+    # Convert 'Päivämäärä' to datetime and handle errors
+    df['Päivämäärä'] = pd.to_datetime(
+        df['Päivämäärä'], dayfirst=True, errors='coerce')
+
+    # Check how many rows have NaT in 'Päivämäärä'
+    nat_count = df['Päivämäärä'].isna().sum()
+    print("Number of NaT values:", nat_count)
+
+    # Drop rows where 'Päivämäärä' is NaT (not a time)
+    df = df.dropna(subset=['Päivämäärä'])
+
+    # Set 'Päivämäärä' as the index
+    df.set_index('Päivämäärä', inplace=True)
+
+    return df
 
 
 def main():
+    df = bicycle_timeseries()
+    print(df.head())
     return None
 
 
