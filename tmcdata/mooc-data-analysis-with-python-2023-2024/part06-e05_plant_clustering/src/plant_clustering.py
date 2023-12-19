563094737f8379ea72ba17cb8014bfabc6ae02da
Elar Saks <elarsaks@gmail.com>
2023-12-19 11:18:01 +0200

    Do plant clustering.

--- tmcdata/mooc-data-analysis-with-python-2023-2024/part06-e05_plant_clustering/src/plant_clustering.py
+++ tmcdata/mooc-data-analysis-with-python-2023-2024/part06-e05_plant_clustering/src/plant_clustering.py
@@ -1,22 +1,43 @@
 #!/usr/bin/env python3
 
 import scipy
+from sklearn.datasets import load_iris
+from sklearn.cluster import KMeans
+from sklearn.metrics import accuracy_score
+import numpy as np
 
 
 def find_permutation(n_clusters, real_labels, labels):
-    permutation=[]
+    permutation = []
     for i in range(n_clusters):
         idx = labels == i
         # Choose the most common label among data points in the cluster
-        new_label=scipy.stats.mode(real_labels[idx])[0][0]
+        new_label = scipy.stats.mode(real_labels[idx])[0][0]
         permutation.append(new_label)
     return permutation
 
+
 def plant_clustering():
-    return 0.0
+    # Load the iris dataset
+    iris = load_iris()
+    X, y = iris.data, iris.target
+
+    # Apply k-means clustering
+    kmeans = KMeans(n_clusters=3, random_state=0)
+    clusters = kmeans.fit_predict(X)
+
+    # Find the best permutation of cluster labels
+    permutation = find_permutation(3, y, clusters)
+    new_labels = [permutation[label] for label in clusters]
+
+    # Calculate and return the accuracy score
+    return accuracy_score(y, new_labels)
+
 
 def main():
-    print(plant_clustering())
+    accuracy = plant_clustering()
+    print(f"Accuracy is {accuracy}")
+
 
 if __name__ == "__main__":
     main()
